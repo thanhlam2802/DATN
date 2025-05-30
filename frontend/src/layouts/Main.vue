af2f025adfd3ce2f576cc42294a451831b305cce
Võ Lê Thanh Lâm <baolethanhlam2001@gmail.com>
2025-05-30 16:52:29 +0700

    merge mainlocal

--- frontend/src/layouts/Main.vue
+++ frontend/src/layouts/Main.vue
@@ -6,12 +6,12 @@ import TourManager from "../layouts/TourManager.vue";
 </script>
 
 <template>
-  <!-- <Header />
+  <Header />
 
   <main class="min-h-[calc(100vh-120px)] flex justify-center px-6">
     <router-view />
   </main>
 
-  <Footer /> -->
-  <TourManager />
+  <Footer />
+  <!-- <TourManager /> -->
 </template>

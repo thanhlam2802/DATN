91213fb07275353066ab09e770bfb36a16ef540f
Thety2005 <thety22112005@gmail.com>
2025-06-03 11:32:53 +0700

    fix/index_router

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,7 +4,7 @@ import Home from "../views/Home.vue";
 import Tour from "../views/Tour.vue";
 import Car from "../views/Car.vue";
 import Plane from "../views/Plane.vue";
-import Hotel from "../views/Hotel.vue";
+import Hotel from "@/views/hotel/Hotel.vue";
 import TourDetail from "../views/TourDetail.vue";
 import Login from "../views/Login.vue";
 import Register from "../views/Register.vue";
@@ -12,13 +12,45 @@ import AccountView from "../views/AccountView.vue";
 import AccountDetails from "../components/User/AccountDetails.vue";
 import PaymentDetails from "../components/User/PaymentDetails.vue";
 import AccountSecurity from "../components/User/AccountSecurity.vue";
+import HotelListingPage from "@/views/hotel/HotelListingPage.vue";
+import HotelDetail from "@/views/hotel/HotelDetail.vue";
+import HotelBooking from "@/views/hotel/HotelBooking.vue";
+import BookingSuccess from "@/views/hotel/BookingSuccess.vue";
 
 const routes = [
   { path: "/", name: "Home", component: Home },
   { path: "/tour", name: "Tour", component: Tour },
   { path: "/car", name: "Car", component: Car },
   { path: "/plane", name: "Plane", component: Plane },
-  { path: "/hotel", name: "Hotel", component: Hotel },
+  {
+    path: "/hotel",
+    name: "Hotel",
+    component: Hotel,
+    children: [
+      {
+        path: '',
+        name: 'HotelListing',
+        component: HotelListingPage,
+      },
+      {
+        path: ':id',
+        name: 'HotelDetail',
+        component: HotelDetail,
+        props: true,
+      },
+      {
+        path: ':id/booking',
+        name: 'HotelBooking',
+        component: HotelBooking,
+        props: true,
+      },
+      {
+        path: ':id/booking/success',
+        name: 'BookingSuccess',
+        component: BookingSuccess,
+      }
+    ],
+  },
   {
     path: "/tours/:id",
     name: "TourDetail",

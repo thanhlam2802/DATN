95c64a9c68bd31921d52ff9a63243a5d90d7a989
Luong Huynh Nam <luonghuynhnam2610@gmail.com>
2025-06-06 21:19:32 +0700

    fix conflix

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -36,9 +36,7 @@ const routes = [
 
     {path: "/plane/pay", name: "PayFlight", component: PayFlight},
     {path: "/plane/getticket", name: "Get ticket", component: GetTicket},
-<<<<<<< HEAD
-    {path: "/plane/admin", name: "Get ticket", component: AdminFight},
-=======
+
 
     {
     path: "/hotel",
@@ -70,7 +68,7 @@ const routes = [
     ],
   },
 
->>>>>>> 3abe751fdf1f1188774e80d14f8973404d96db28
+
     {
         path: "/tours/:id",
         name: "TourDetail",

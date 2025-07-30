32ba320c7caa66dcee5ebd265b5078450c29ea54
minhtrang7011 <trangntmps39400@gmail.com>
2025-07-30 23:22:25 +0700

    Remove code

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,21 +1,5 @@
 <script setup>
 import ToastContainer from '@/components/ToastContainer.vue';
-import {onMounted} from "vue";
-import {AccountApi} from "@/api/AccountApi.js";
-import {useUserStore} from "@/store/UserStore.js";
-import {useRouter} from "vue-router";
-import {ErrorCodes} from "@/data/ErrorCode.js";
-
-const userStore = useUserStore();
-const router = useRouter();
-
-onMounted(async () => {
-  const res = await AccountApi.getProfile()
-  console.log("res", res)
-  if (res["errorCode"] === ErrorCodes.userNotVerified) {
-    await router.push("/verify-email")
-  }
-})
 </script>
 
 <template>
